f2288271cebae13e437b72d235fd5e2e1218919a
antoniopenizollo <antoniopenizollo@gmail.com>
2020-09-24 10:23:33 -0300

    criando operação de instanciação

--- src/main/java/com/course/SpringCourseApplication.java
+++ src/main/java/com/course/SpringCourseApplication.java
@@ -1,13 +1,31 @@
 package com.course;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringCourseApplication {
+import com.course.domain.Categoria;
+import com.course.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringCourseApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCourseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"informatica");
+		Categoria cat2 = new Categoria(null,"escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
