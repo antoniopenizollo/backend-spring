3d86df28bcca2bbe2f879027c619797f78e74d36
Antônio Penizollo <antoniopenizollo@gmail.com>
2021-04-27 21:20:25 -0300

    Email HTML

--- src/main/java/com/course/services/EmailService.java
+++ src/main/java/com/course/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.course.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.course.domain.Pedido;
@@ -10,4 +12,7 @@ public interface EmailService {
 	
 	void sendEmail(SimpleMailMessage msg);
 	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/course/services/PedidoService.java
+++ src/main/java/com/course/services/PedidoService.java
@@ -40,6 +40,7 @@ public class PedidoService {
 	@Autowired
 	private EmailService emailService;
 	
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -66,7 +67,7 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 }
